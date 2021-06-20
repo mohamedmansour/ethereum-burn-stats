9842d4e2b644bc028495ff5ccedc0c1fd3a1eecd
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-20 19:47:07 +0000

    Add page navigations between blocks

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -10,14 +10,33 @@ import React from "react";
 interface PageTitleProps extends HTMLChakraProps<"header"> {
   title: string;
   subtitle: string;
+  beforeRender?: JSX.Element;
+  afterRender?: JSX.Element;
 }
 
 export const PageTitle = forwardRef<PageTitleProps, "div">(
   (props: PageTitleProps, ref: React.ForwardedRef<any>) => {
+    const {
+      title,
+      subtitle,
+      beforeRender,
+      afterRender,
+      ...rest
+    } = props
+
+    const styles = {
+      title: props.title,
+      subtitle: props.subtitle,
+      beforeRender: props.beforeRender,
+      afterRender: props.afterRender
+    }
+
     return (
-      <HStack ref={ref} {...props}>
-        <Heading size="md">{props.title}</Heading>
-        <Text>{props.subtitle}</Text>
+      <HStack ref={ref} {...rest}>
+        {styles.beforeRender && styles.beforeRender}
+        <Heading size="md">{styles.title}</Heading>
+        <Text>{styles.subtitle}</Text>
+        {styles.afterRender && styles.afterRender}
       </HStack>
     );
   }
--- src/pages/EthBlockDetail.tsx
+++ src/pages/EthBlockDetail.tsx
@@ -8,41 +8,94 @@ import {
   Td,
   Link,
   VStack,
+  Icon,
 } from "@chakra-ui/react";
 import { BlockWithTransactions } from "@ethersproject/abstract-provider";
 import { utils } from "ethers";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useReducer, useEffect } from "react";
+import { VscChevronLeft, VscChevronRight } from "react-icons/vsc";
+import { useParams, Link as ReactLink, useHistory } from "react-router-dom";
 import { Loader } from "../components/Loader";
 import { useEthereum } from "../contexts/EthereumContext";
 import { Setting } from "../contexts/SettingsContext";
 import { useSetting } from "../hooks/useSetting";
-import { Link as ReactLink } from "react-router-dom";
 import { Card } from "../components/Card";
 import { PageTitle } from "../components/PageTitle";
 
+interface BlockDetailState {
+  block?: BlockWithTransactions;
+  onBeforeRender?: boolean;
+  onAfterRender?: boolean;
+}
+
+interface UpdateAction extends BlockDetailState {
+  type: "UPDATE";
+}
+
+type BlockDetailAction = UpdateAction;
+
+const blockDetailReducer = (
+  state: BlockDetailState,
+  action: BlockDetailAction
+): BlockDetailState => {
+  switch (action.type) {
+    case "UPDATE":
+      return action;
+  }
+};
+
 export function EthBlockDetail() {
+  let history = useHistory();
   let { id } = useParams<{ id: string }>();
   const { eth } = useEthereum();
-  const [block, setBlock] = useState<BlockWithTransactions>();
   const formatBurnInGwei = useSetting<boolean>(Setting.formatBurnInGwei);
+  const [state, dispatch] = useReducer(blockDetailReducer, {});
 
   useEffect(() => {
-    if (!eth) return;
-
     (async () => {
-      setBlock(await eth.getBlockWithTransactions(parseInt(id)));
+      const blockNumber = parseInt(id);
+      if (!eth || !id) return;
+      const latestBlockNumber = await eth.getBlockNumber();
+      dispatch({
+        type: "UPDATE",
+        block: await eth.getBlockWithTransactions(blockNumber),
+        onBeforeRender: blockNumber > 0,
+        onAfterRender: latestBlockNumber > blockNumber,
+      });
     })();
   }, [eth, id]);
 
-  if (!block) {
+  const { block } = state;
+
+  if (!block || !eth) {
     return <Loader>Loading transactions for block</Loader>;
   }
 
+  const onBeforeRender = state.onBeforeRender ? (
+    <Icon
+      userSelect="none"
+      as={VscChevronLeft}
+      onClick={() => history.push(`/block/${parseInt(id) - 1}`)}
+    />
+  ) : undefined;
+
+  const onAfterRender = state.onAfterRender ? (
+    <Icon
+      userSelect="none"
+      as={VscChevronRight}
+      onClick={() => history.push(`/block/${parseInt(id) + 1}`)}
+    />
+  ) : undefined;
+
   return (
     <VStack overflow="hidden" m="4" mt="0" align="flex-start" h="100%">
-      <PageTitle title="Block" subtitle={"#" + id} />
+      <PageTitle
+        title="Block"
+        subtitle={"#" + id}
+        beforeRender={onBeforeRender}
+        afterRender={onAfterRender}
+      />
+
       <Card overflow="auto" flex="1" w="100%">
         {block.transactions.length === 0 && <Text>No Transactions</Text>}
         {block.transactions.length !== 0 && (
