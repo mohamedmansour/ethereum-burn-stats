5df88a955e6bd43f0f66820fb66652ae65fcf701
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-21 10:10:32 +0000

    Componetize FirePit

--- src/components/FirePit.tsx
+++ src/components/FirePit.tsx
@@ -1,31 +1,39 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, forwardRef, HTMLChakraProps } from '@chakra-ui/react'
 import * as CSS from "csstype";
 import './FirePit.scss'
 
-export interface FirePitProps {
+export interface FirePitProps extends HTMLChakraProps<"div">  {
   sparkCount?: number
   size?: CSS.Property.Width;
 }
 
-export function FirePit(props: FirePitProps) {
-  const sparkCount = props.sparkCount || 0
-  const size = props.size || '40px'
+export const FirePit = forwardRef<FirePitProps, "div">(
+    (props: FirePitProps, ref: React.ForwardedRef<any>) => {
+  const {
+    sparkCount,
+    size,
+    ...rest
+  } = props
+
+  const styles = {
+    sparkCount: sparkCount || 0,
+    size: size || '40px'
+  }
 
   const sparks = []
-  for (let i = 0; i < sparkCount; i++) {
+  for (let i = 0; i < styles.sparkCount; i++) {
     sparks.push(<Box key={i} className="spark" />)
   }
+
   return (
-    <Flex position="relative" w="100%" h="100%" justifyContent="center" alignItems="flex-end">
-      <Box className="firepit" w={size} h={size} >
-        <Box className="fire">
-          <Box className="flame" />
-          <Box className="flame" />
-          <Box className="flame" />
-          <Box className="flame" />
-          {sparks}
-        </Box>
+    <Box className="firepit" fontSize={styles.size} {...rest}>
+      <Box className="fire">
+        <Box className="flame" />
+        <Box className="flame" />
+        <Box className="flame" />
+        <Box className="flame" />
+        {sparks}
       </Box>
-    </Flex>
+    </Box>
   )
-}
+})
