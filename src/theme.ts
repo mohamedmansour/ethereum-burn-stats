cd1a47bba37ebff6ab4eed64708a3ffe82c6caa9
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-21 10:11:27 +0000

    Add more theme variables

--- src/theme.ts
+++ src/theme.ts
@@ -3,14 +3,18 @@ import { extendTheme } from "@chakra-ui/react"
 const theme = extendTheme({
   colors: {
     brand: {
-      header: 'hsl(211deg 21% 39%)',
-      subheader: 'hsl(211deg 21% 42%)',
-      subheaderCard: 'hsl(212deg 21% 34%)',
-      subheaderCardText: '#e6e6e6',
-      background: '#EDF2F7',
+      header: 'hsl(0deg 0% 0% / 50%)',
+      headerText: '#666',
+      subheader: '#333',
+      subheaderCard: 'rgb(34, 34, 34, 0.6)',
+      subheaderCardText: 'white',
+      background: '#333',
       settings: 'hsl(212deg 21% 32%)',
       settingsOutline: 'hsl(212deg 21% 39%)',
-      card: '#ffffff'
+      card: 'rgb(34, 34, 34, 0.6)',
+      primaryText: 'white',
+      secondaryText: '#7d7d7d',
+      linkColor: 'rgb(0, 108, 190)'
     },
   },
 })
