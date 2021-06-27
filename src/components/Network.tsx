4c304dbb7b4463041957623b3461f0d1258b5699
Mohamed Mansour <hello@mohamedmansour.com>
2021-06-27 19:46:26 +0000

    Use Ethereum Context to query Network instead of Settings

--- src/components/Network.tsx
+++ src/components/Network.tsx
@@ -1,13 +1,14 @@
 import { Button, forwardRef, HTMLChakraProps, Icon } from "@chakra-ui/react";
 import { SiEthereum } from "react-icons/si";
 import { useHistory } from "react-router-dom";
-import { Setting, EthereumNetwork } from "../config";
-import { useSetting } from "../hooks/useSetting";
+import { useEthereum } from "../contexts/EthereumContext";
 
 export const EthereumNetworkBadge = forwardRef<HTMLChakraProps<"button">, "button">(
     (props: HTMLChakraProps<"button">, ref: React.ForwardedRef<any>) => {
   const history = useHistory()
-  const network = useSetting<EthereumNetwork>(Setting.network);
+  const eth = useEthereum();
+  
+  const name = eth.eth?.connectedNetwork.name || 'Connecting to network...'
   return (
     <Button
       colorScheme="orange"
@@ -18,7 +19,7 @@ export const EthereumNetworkBadge = forwardRef<HTMLChakraProps<"button">, "butto
       onClick={() => history.push('/settings')}
       {...props}
     >
-      {network.name}
+      {name}
     </Button>
   );
 });
