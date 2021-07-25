815e977bd87ecec7c9b3ab85d419c6f25d9d2b92
Mohamed Mansour <hello@mohamedmansour.com>
2021-07-25 05:24:30 -0700

    Remove websocket proxying, it never worked.

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,19 +1,7 @@
-const createProxyMiddleware = require('http-proxy-middleware').createProxyMiddleware;
-
 let price = 1900.12
 
 module.exports = function(app) {
-  const socketProxy = createProxyMiddleware('/ws', {
-    target: 'ws://127.0.0.1:8546',
-    ws: true,
-    changeOrigin: true,
-  });
-  app.listen().on('upgrade', socketProxy.upgrade)
 
-  app.use(
-    '/ws',
-    socketProxy
-  )
 
   app.get('/price.json',(req, res) => {
     price = price + 5
