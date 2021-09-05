4acb6db88b94b5db4e01fa63a2a0c3f06b260058
Mohamed Mansour <hello@mohamedmansour.com>
2021-09-04 17:02:01 -0700

    Revert "Add label to color mode switcher to assist users it exists"
    This reverts commit 7b95c7fc646c2b36aabc5559d516466b70ada4e1.

--- frontend/src/atoms/ColorModeSwitcher.tsx
+++ frontend/src/atoms/ColorModeSwitcher.tsx
@@ -4,10 +4,8 @@ import {
   useColorModeValue,
   IconButton,
   IconButtonProps,
-  Button,
 } from "@chakra-ui/react"
 import { IoMdMoon, IoMdSunny } from "react-icons/io"
-import { Setting } from "../config"
 
 type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">
 
@@ -16,22 +14,17 @@ export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
   const text = useColorModeValue("dark", "light")
   const SwitchIcon = useColorModeValue(IoMdMoon, IoMdSunny)
 
-  const onSwitchClick = () => {
-    toggleColorMode()
-  }
-
-  const label = `Switch to ${text} mode`
-  const buttonStyle: any = {
-    fontSize:"md",
-    variant:"ghost",
-    color:"current",
-    marginLeft:"2",
-    onClick: onSwitchClick,
-    "aria-label": label
-  }
-
-  if (localStorage[Setting.colorMode])
-    return <IconButton icon={<SwitchIcon />} {...buttonStyle} {...props}>{label}</IconButton>
-  else
-    return <Button rightIcon={<SwitchIcon />} {...buttonStyle} {...props}>{label}</Button>
+  return (
+    <IconButton
+      size="md"
+      fontSize="lg"
+      variant="ghost"
+      color="current"
+      marginLeft="2"
+      onClick={toggleColorMode}
+      icon={<SwitchIcon />}
+      aria-label={`Switch to ${text} mode`}
+      {...props}
+    />
+  )
 }
