4aaea3fb95ae75317d497bfa0af60e1f089487a1
Mohamed Mansour <hello@mohamedmansour.com>
2021-09-10 01:36:05 -0700

    Add frontier to our newest patrons! (#89)
    * Add frontier to our newest patrons!
    
    * Fix warnings

--- frontend/src/pages/Dashboard/CardDonate.tsx
+++ frontend/src/pages/Dashboard/CardDonate.tsx
@@ -13,11 +13,39 @@ import {
 } from "@chakra-ui/react";
 import { Card } from "../../atoms/Card";
 import { LogoGoodGhosting, LogoIndexed, LogoNansen, LogoUnicornCircle } from "./Logos";
+import { LogoFrontier } from "./Logos/LogoFrontier";
 
 export enum CardDonateType {
   TopSideBar,
   BottomSideBar
 }
+
+const Patrons = [
+  {
+    id: 'indexed',
+    logo: LogoIndexed,
+    url: "https://indexed.finance",
+    description: "indexed.finance: defi investing made easy, crypto indexes and yield aggregators"
+  },
+  {
+    id: 'unicorn',
+    logo: LogoUnicornCircle,
+    url: "https://twitter.com/unicorncircle" ,
+    description: "Unicorn Circle: The one and only Unicorn Circle 🦄⭕️ | Always on the hunt for new Business Unicorns!"
+  },
+  {
+    id: 'goodghosting',
+    logo: LogoGoodGhosting,
+    url: "https://goodghosting.com",
+    description: "GoodGhosting: The new addictive way to save. Our savings pools reward regular savers with higher interest rates. Start building the financial habits you deserve."
+  },
+  {
+    id: 'frontier',
+    logo: LogoFrontier,
+    url: "https://frontier.xyz/",
+    description: "Frontier: Native, seamless, cross-chain DeFi aggregation layer"
+  }
+]
 
 export function CardDonate({ type }: { type: CardDonateType }) {
   const logoColor = useColorModeValue("#001D32", "white")
@@ -40,29 +68,17 @@ export function CardDonate({ type }: { type: CardDonateType }) {
       )
     }
 
-    const indexedTitle = "indexed.finance: defi investing made easy, crypto indexes and yield aggregators"
-    const unicornCircleTitle = "The one and only Unicorn Circle 🦄⭕️ | Always on the hunt for new Business Unicorns!"
-    const goodghostingTitle = "The new addictive way to save. Our savings pools reward regular savers with higher interest rates. Start building the financial habits you deserve."
-    
     return (
       <Card title="Patrons" subtitle="Thanks to these patrons who has offered to help support the server costs and development!">
         <Box>
           <SimpleGrid columns={2} gridGap={2} mt={2} alignItems="center" justifyContent="center">
-            <LinkBox title={indexedTitle}>
-              <LinkOverlay href="https://indexed.finance" target="_blank">
-                <LogoIndexed color={logoColor} />
-              </LinkOverlay>
-            </LinkBox>
-            <LinkBox title={unicornCircleTitle}>
-              <LinkOverlay href="https://twitter.com/unicorncircle" target="_blank">
-                <LogoUnicornCircle color={logoColor} />
-              </LinkOverlay>
-            </LinkBox>
-            <LinkBox title={goodghostingTitle}>
-              <LinkOverlay href="https://goodghosting.com" target="_blank">
-                <LogoGoodGhosting color={logoColor} />
-              </LinkOverlay>
-            </LinkBox>
+            {Patrons.map(patron => (
+              <LinkBox title={patron.description} key={patron.id}>
+                <LinkOverlay href={patron.url} target="_blank">
+                  <patron.logo color={logoColor} />
+                </LinkOverlay>
+              </LinkBox>
+            ))}
           </SimpleGrid>
           <Text fontSize={12} textAlign="right"><Link href="https://twitter.com/mohamedmansour" target="_blank">Become a patron!</Link></Text>
         </Box>
@@ -75,7 +91,7 @@ export function CardDonate({ type }: { type: CardDonateType }) {
   }
 
   return (
-    <Card title="Current Session">
+    <Card title="Support WatchTheBurn">
       <Box pl={4} pr={4} pb={4}>
         <Text>It's expensive hosting multiple geth instances on the cloud. Any help would be appreciated:</Text>
         <UnorderedList mt={4}>
